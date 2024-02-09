c6d0cbd68e6009457617155a837fc6c39adb0652
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-02-09 08:44:55 +0200

    Up

--- my_app/__manifest__.py
+++ my_app/__manifest__.py
@@ -7,6 +7,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/my_mode_view.xml',
+        'views/res_config_settings_views.xml',
         'data/ir_cron.xml',
         'data/ir_actions_server.xml',
         'wizard/my_wizard_view.xml',
