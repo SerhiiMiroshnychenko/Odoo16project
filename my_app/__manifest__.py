0265238e077c3a1f67d9b9e6ccb9298c34004162
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-02-10 10:10:41 +0200

    Up

--- my_app/__manifest__.py
+++ my_app/__manifest__.py
@@ -3,15 +3,17 @@
     'description': '',
     'summary': '',
     'license': 'LGPL-3',
-    'depends': ['base', 'mail', 'website'],
+    'depends': ['base', 'mail', 'website', 'sale_management'],
     'data': [
         'security/ir.model.access.csv',
         'views/my_mode_view.xml',
         'views/res_config_settings_views.xml',
+        'views/sale_order_views.xml',
         'data/ir_cron.xml',
         'data/ir_actions_server.xml',
         'wizard/my_wizard_view.xml',
         'templates/website_templates.xml',
+        'reports/sale_order_report.xml',
         'reports/my_report.xml',
     ],
     'assets': {
